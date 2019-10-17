8177c1342988ec8388c2f195c6ca142c8d91da4f
Adam Dressel <dresseladam3@users.noreply.github.com>
2019-10-17 15:26:35 -0400

    Added UI for main menu, edit for Controller Class

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,37 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+  @FXML
+  private Button adminBtn;
+  @FXML
+  private Button statsBtn;
+  @FXML
+  private Button versusBtn;
+  @FXML
+  private Button tournBtn;
+
+
+  public void initialize() {
+
+  }
+
+  public void openVersusMenu() {
+
+  }
+
+  public void openTournMenu() {
+
+  }
+
+  public void openStatsMenu() {
+
+  }
+
+  public void openAdminMenu() {
+
+  }
 }
