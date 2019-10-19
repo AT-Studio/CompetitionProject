4bdd4a2429665daf3fa107a585cb417092133103
nolzaj93 <nolzaj93@gmail.com>
2019-10-19 15:30:28 -0400

    Formatting

--- src/sample/ScoreEntryController.java
+++ src/sample/ScoreEntryController.java
@@ -1,5 +1,6 @@
 package sample;
 
+import java.util.InputMismatchException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -18,10 +19,22 @@ public class ScoreEntryController {
   private Label user2Label;
 
   @FXML
-  private TextField user1Score;
+  private Label user1Score;
 
   @FXML
-  private TextField user2Score;
+  private Label user2Score;
+
+  @FXML
+  private TextField enteredScore1;
+
+  @FXML
+  private TextField enteredScore2;
+
+  @FXML
+  private Label returnMsg1;
+
+  @FXML
+  private Label returnMsg2;
 
   @FXML
   public void returnToMenuFromScore(ActionEvent event) {
@@ -37,8 +50,40 @@ public class ScoreEntryController {
   }
 
   public void iniitialize() {
-
+    // setText() of labels to usernames of user1 and user2
 
   }
 
+  @FXML
+  public void updateEnteredScore1() {
+
+    updateEnteredScore(enteredScore1, user1Score, returnMsg1);
+  }
+
+  @FXML
+  public void updateEnteredScore2() {
+
+    updateEnteredScore(enteredScore2, user2Score, returnMsg2);
+  }
+
+  private void updateEnteredScore(TextField enteredScore2, Label user2Score, Label returnMsg2) {
+    try {
+      int score2 = Integer.parseInt(enteredScore2.getText());
+
+      if (score2 >= 0 && score2 <= 11) {
+        user2Score.setText(Integer.toString(score2));
+        returnMsg2.setVisible(false);
+      } else {
+        returnMsg2.setVisible(true);
+        returnMsg2.setText("Please enter a score between 0 and 11.");
+      }
+
+    } catch (NumberFormatException ex) {
+      returnMsg2.setVisible(true);
+      returnMsg2.setText("Invalid score. Please enter a number 0-11.");
+    }
+    catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
 }
