dc173be9488842c9d51bb0c9c1c74d927b44a680
nolzaj93 <nolzaj93@gmail.com>
2019-10-19 16:42:38 -0400

    Added admin panel

---
+++ src/sample/AdminPanelController.java
@@ -0,0 +1,18 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AdminPanelController {
+
+  @FXML
+  private TextField adminName;
+
+  @FXML
+  private TextField adminPW;
+
+  public void initialize() {
+
+  }
+
+}
