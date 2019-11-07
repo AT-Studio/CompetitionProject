5d1b2a594f483ba8ca591985ef9eeefb83e7874e
nolzaj93 <nolzaj93@gmail.com>
2019-11-07 15:04:24 -0500

    Documented AdminPanelController

--- src/sample/AdminPanelController.java
+++ src/sample/AdminPanelController.java
@@ -1,13 +1,17 @@
 package sample;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+/**
+ * The AdminPanelController class is the controller used to listen for user events in the
+ * admin-panel.fxml view.
+ *
+ * @author austinnolz - The Admin Panel will show the Tournament Manager button and the Search User
+ * button if an administrator successfully logs in. Pressing the Tournament Manager button will
+ * show admin-tourn-manager.fxml and pressing the searchUserBtn will show searchProf.fxml.
+ */
 public class AdminPanelController {
 
   @FXML
@@ -26,20 +30,34 @@ public class AdminPanelController {
 
   }
 
+  /**
+   * The openTournManager() method calls loadNewView in the Main class which switches the Scene
+   * to admin-tourn-manager.fxml.
+   */
   @FXML
-  public void openTournManager() throws Exception {
+  public void openTournManager() {
     Main.loadNewView("admin-tourn-manager.fxml");
   }
 
+  /**
+   * Calls loadNewView() which switches the scene
+   */
   @FXML
   public void openUserSearch() {
     Main.loadNewView("searchProf.fxml");
   }
+
+  /**
+   * Calls loadMainMenu() which switches the scene to main-menu.fxml.
+   */
   @FXML
-  public void returnToMenu(ActionEvent event) {
+  public void returnToMenu() {
     Main.loadMainMenu();
   }
 
+  /**
+   * The verifyAdmin() method checks if the entered username and password match any admin profile.
+   */
   @FXML
   public void verifyAdmin(){
 
